a60eabf9fc002ef9624a77066489493b5056e0d1
Codesat45 <satyamtripathi29nov2000@gmail.com>
2026-02-05 01:47:24 +0530

    frontend fixed

--- Ecommerce/frontpart/vite.config.js
+++ Ecommerce/frontpart/vite.config.js
@@ -1,14 +1,9 @@
-<<<<<<< HEAD
-=======
-// vite.config.js
->>>>>>> c9f00cd2e91e36c727d2a0be3dde43e4f35d4765
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-<<<<<<< HEAD
+
   plugins: [react()]
-=======
-  plugins: [react()],
->>>>>>> c9f00cd2e91e36c727d2a0be3dde43e4f35d4765
+
 });
