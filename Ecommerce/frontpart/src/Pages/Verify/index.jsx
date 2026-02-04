70c10cb89bb8b86285e7e01260e3311c9d60a812
Codesat45 <satyamtripathi29nov2000@gmail.com>
2026-02-05 01:50:43 +0530

    frontend fixed again

--- Ecommerce/frontpart/src/Pages/Verify/index.jsx
+++ Ecommerce/frontpart/src/Pages/Verify/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import img from "../../assets/sheild1.png";
 import OtpBox from "../../components/OtpBox";
